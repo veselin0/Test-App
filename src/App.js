1280f21c437d029aa963512f916f9816a80adfe1
veselin0 <vesselin60@gmail.com>
2021-11-29 16:22:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const Gocho = ({name, surname, link}) => {
+    return (
+        <div>
+            <h1> My name is {name()}, surname - {surname} </h1>
+            <a href={link}>My profile</a>
+        </div>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Gocho name={() => {return 'Gocho'}} surname="Kochev" link="twitter.com"/>
+            <Gocho name={() => {return 'Mocho'}} surname="Bochev" link="twitter.com"/>
+        </div>
+    );
 }
 
 export default App;
