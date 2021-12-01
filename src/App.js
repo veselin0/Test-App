19ec994f4905e55c795595d45d6daeaba18f4245
veselin0 <vesselin60@gmail.com>
2021-12-01 15:34:13 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
+import { Component } from "react";
+
 import "./App.css";
 
-const Gocho = ({name, surname, link}) => {
-    return (
-        <div>
-            <h1> My name is {name()}, surname - {surname} </h1>
-            <a href={link}>My profile</a>
-        </div>
-    );
+class Gocho extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 27,
+            text: '+++'
+        }
+    }
+
+    nextYear = () => {
+        console.log('+++');
+        this.setState(state => ({
+            years: state.years + 1
+        }));
+    }
+
+    render() {
+        const {name, surname, link} = this.props;
+        return (
+            <div>
+                <button onClick={this.nextYear}>{this.state.text}</button>
+                <h1> My name is {name}, surname - {surname}, age - {this.state.years} </h1>
+                <a href={link}>My profile</a>
+            </div>
+        );
+    }
+    
 };
 
 function App() {
     return (
         <div className="App">
-            <Gocho name={() => {return 'Gocho'}} surname="Kochev" link="twitter.com"/>
-            <Gocho name={() => {return 'Mocho'}} surname="Bochev" link="twitter.com"/>
+            <Gocho name= 'Gocho' surname="Kochev" link="twitter.com"/>
+            <Gocho name='Mocho' surname="Bochev" link="twitter.com"/>
         </div>
     );
 }
