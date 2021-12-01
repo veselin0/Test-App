d5b6cea6dbc9c5622603560c6d729bef62751161
veselin0 <vesselin60@gmail.com>
2021-12-01 16:13:38 +0200

    Events and this

--- src/App.js
+++ src/App.js
@@ -7,24 +7,46 @@ class Gocho extends Component {
         super(props);
         this.state = {
             years: 27,
-            text: '+++'
+            text: '+++',
+            position: ''
         }
+        this.nextYear = this.nextYear.bind(this);//using buind
     }
 
-    nextYear = () => {
-        console.log('+++');
+    //using bind:
+    nextYear() {
         this.setState(state => ({
             years: state.years + 1
         }));
     }
 
+    commitInputChanges = (e, color) => {
+        console.log(color);
+        this.setState({
+            position: e.target.value
+        });
+    }
+
     render() {
         const {name, surname, link} = this.props;
+        const {position, years, text} = this.state;
+
+        console.log(this);
+
         return (
             <div>
-                <button onClick={this.nextYear}>{this.state.text}</button>
-                <h1> My name is {name}, surname - {surname}, age - {this.state.years} </h1>
+                <button onClick={this.nextYear}>{text}</button>
+                <h1> 
+                    My name is {name}, 
+                    surname - {surname}, 
+                    age - {years}, 
+                    position - {position} 
+                </h1>
                 <a href={link}>My profile</a>
+                <form>
+                    <span>Insert Position</span>
+                    <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+                </form>
             </div>
         );
     }
